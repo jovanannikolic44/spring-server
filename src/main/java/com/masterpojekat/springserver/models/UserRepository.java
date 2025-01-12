844eb588e48f408c9c650bd5b2a60fe784033fdb
Jovana Nikolic <jovana.nikolic452@gmail.com>
2025-01-12 21:20:13 +0100

    Use crud repository

--- src/main/java/com/masterpojekat/springserver/models/UserRepository.java
+++ src/main/java/com/masterpojekat/springserver/models/UserRepository.java
@@ -1,10 +1,9 @@
 package com.masterpojekat.springserver.models;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, String> {
 
 }
